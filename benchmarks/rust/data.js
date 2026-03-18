c5cff60f62e2a62985252f2792e7c2be9abbbe6d
github-action-benchmark <github@users.noreply.github.com>
2026-03-18 14:55:12 +0000

    add Rust Benchmarks (customSmallerIsBetter) benchmark result for e30aeed1ce7eedc9b57e082142c96c40438298e6

--- benchmarks/rust/data.js
+++ benchmarks/rust/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773844683752,
+  "lastUpdate": 1773845712654,
   "repoUrl": "https://github.com/aramirez087/TuitBot",
   "entries": {
     "Rust Benchmarks": [
@@ -260,6 +260,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "trivial_operation",
             "value": 933.28,
+            "unit": "ps"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alexramirez.cr@gmail.com",
+            "name": "Alexander Ramirez Kiriushenko",
+            "username": "aramirez087"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e30aeed1ce7eedc9b57e082142c96c40438298e6",
+          "message": "Merge pull request #215 from aramirez087/ci/release-verify\n\nci: add release-verify job to validate crate packaging",
+          "timestamp": "2026-03-18T08:52:49-06:00",
+          "tree_id": "5c742f39f226f887d3fb34e85ff2d93e78f242cd",
+          "url": "https://github.com/aramirez087/TuitBot/commit/e30aeed1ce7eedc9b57e082142c96c40438298e6"
+        },
+        "date": 1773845712125,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "trivial_operation",
+            "value": 932.84,
             "unit": "ps"
           }
         ]
