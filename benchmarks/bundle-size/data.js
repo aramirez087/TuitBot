beb31b8daa612d7beb12461bda463113e45e9dd5
github-action-benchmark <github@users.noreply.github.com>
2026-03-19 22:00:52 +0000

    add Frontend Bundle Size (customSmallerIsBetter) benchmark result for bc98689c1634d832b94e72bfbbd1d041d2e0d676

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773957626613,
+  "lastUpdate": 1773957652301,
   "repoUrl": "https://github.com/aramirez087/TuitBot",
   "entries": {
     "Frontend Bundle Size": [
@@ -660,6 +660,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Total build",
             "value": 1208052,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alexramirez.cr@gmail.com",
+            "name": "Alexander Ramirez Kiriushenko",
+            "username": "aramirez087"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc98689c1634d832b94e72bfbbd1d041d2e0d676",
+          "message": "Merge pull request #223 from aramirez087/dependabot/npm_and_yarn/dashboard/all-dependencies-6466c309cd\n\nchore(deps): dashboard: bump the all-dependencies group in /dashboard with 3 updates",
+          "timestamp": "2026-03-19T16:00:05-06:00",
+          "tree_id": "64d88eab0a7c6148c04550c86b33362200e55967",
+          "url": "https://github.com/aramirez087/TuitBot/commit/bc98689c1634d832b94e72bfbbd1d041d2e0d676"
+        },
+        "date": 1773957651295,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "JS (minified)",
+            "value": 872234,
+            "unit": "bytes"
+          },
+          {
+            "name": "CSS (minified)",
+            "value": 296607,
+            "unit": "bytes"
+          },
+          {
+            "name": "Total build",
+            "value": 1208092,
             "unit": "bytes"
           }
         ]
