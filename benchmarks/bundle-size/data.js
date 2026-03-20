414bf868a1a2c9007b4cac2b4b0ac998de7da95e
github-action-benchmark <github@users.noreply.github.com>
2026-03-20 02:46:38 +0000

    add Frontend Bundle Size (customSmallerIsBetter) benchmark result for 16b04ef9baa5541c39fd623cde100797b6b9fb2b

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773973746037,
+  "lastUpdate": 1773974798572,
   "repoUrl": "https://github.com/aramirez087/TuitBot",
   "entries": {
     "Frontend Bundle Size": [
@@ -1074,6 +1074,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/aramirez087/TuitBot/commit/4e46f856389726158f51c328175b58f016744b7b"
         },
         "date": 1773973745062,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "JS (minified)",
+            "value": 872230,
+            "unit": "bytes"
+          },
+          {
+            "name": "CSS (minified)",
+            "value": 296607,
+            "unit": "bytes"
+          },
+          {
+            "name": "Total build",
+            "value": 1208087,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alexramirez.cr@gmail.com",
+            "name": "Alexander Ramirez Kiriushenko",
+            "username": "aramirez087"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "16b04ef9baa5541c39fd623cde100797b6b9fb2b",
+          "message": "Merge pull request #237 from aramirez087/release-plz-2026-03-20T02-27-44Z\n\nchore: release",
+          "timestamp": "2026-03-19T20:45:51-06:00",
+          "tree_id": "d165ac115c0853c7d94adb24869ace565dba96a8",
+          "url": "https://github.com/aramirez087/TuitBot/commit/16b04ef9baa5541c39fd623cde100797b6b9fb2b"
+        },
+        "date": 1773974797827,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
