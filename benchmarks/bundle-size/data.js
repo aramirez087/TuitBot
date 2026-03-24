fb1dee872565c2d483efa4d130eff3016d9c63fd
github-action-benchmark <github@users.noreply.github.com>
2026-03-24 09:26:32 +0000

    add Frontend Bundle Size (customSmallerIsBetter) benchmark result for 3466e3b7411496750e1aa7a5aeddc4743fdab007

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774316800102,
+  "lastUpdate": 1774344392016,
   "repoUrl": "https://github.com/aramirez087/TuitBot",
   "entries": {
     "Frontend Bundle Size": [
@@ -3310,6 +3310,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Total build",
             "value": 1349367,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alexramirez.cr@gmail.com",
+            "name": "Alexander Ramirez Kiriushenko",
+            "username": "aramirez087"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3466e3b7411496750e1aa7a5aeddc4743fdab007",
+          "message": "feat: wire up semantic search in server and desktop app (#296)",
+          "timestamp": "2026-03-24T03:25:42-06:00",
+          "tree_id": "85c6dc02dbc7ed66f2b8b8670690d6cc5e90bbad",
+          "url": "https://github.com/aramirez087/TuitBot/commit/3466e3b7411496750e1aa7a5aeddc4743fdab007"
+        },
+        "date": 1774344391363,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "JS (minified)",
+            "value": 963655,
+            "unit": "bytes"
+          },
+          {
+            "name": "CSS (minified)",
+            "value": 346465,
+            "unit": "bytes"
+          },
+          {
+            "name": "Total build",
+            "value": 1349370,
             "unit": "bytes"
           }
         ]
